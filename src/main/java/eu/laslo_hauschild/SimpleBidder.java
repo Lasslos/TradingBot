2f27fd312318d8a070178c7121cb640ff4d3772d
Laslo Hauschild <laslo.hauschild@web.de>
2025-06-06 16:59:05 +0200

    added simple bidders

---
+++ src/main/java/eu/laslo_hauschild/SimpleBidder.java
@@ -0,0 +1,24 @@
+package eu.laslo_hauschild;
+
+import auction.Bidder;
+
+/**
+ * There will be [quantity / 2] bidding rounds. Bid [cash / quantity * 2] in each round.
+ */
+public class SimpleBidder implements Bidder {
+    private int bid;
+
+    @Override
+    public void init(int quantity, int cash) {
+        this.bid = (cash / quantity) * 2;
+    }
+
+    @Override
+    public int placeBid() {
+        return bid;
+    }
+
+    @Override
+    public void bids(int own, int other) {
+    }
+}
---
+++ src/main/java/eu/laslo_hauschild/TitForTatBidder.java
@@ -0,0 +1,37 @@
+package eu.laslo_hauschild;
+
+import auction.Bidder;
+
+/**
+ * Places the same bid + 1 as the other bidder in the previous round.
+ */
+public class TitForTatBidder implements Bidder {
+    private int quantity;
+    private int cash;
+
+    // Initialize to -1 in first round. This way, the first bid will be 0.
+    private int lastOtherBid = -1;
+
+    @Override
+    public void init(int quantity, int cash) {
+        this.cash = cash;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int placeBid() {
+        // If we can't afford our usual bid, bid all cash that is left.
+        if (lastOtherBid + 1 > cash) {
+            return cash;
+        }
+
+        return lastOtherBid + 1;
+    }
+
+    @Override
+    public void bids(int own, int other) {
+        quantity -= 2;
+        cash -= own;
+        this.lastOtherBid = other;
+    }
+}
