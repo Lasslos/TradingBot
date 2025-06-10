6f3c0c67b9419aed131fac1656794f5cb8a963ad
Laslo Hauschild <laslo.hauschild@web.de>
2025-06-10 16:38:43 +0200

    added RandomBidder2.java

--- src/main/java/auction/bidders/RandomBidder2.java
+++ src/main/java/auction/bidders/RandomBidder2.java
@@ -1,24 +1,36 @@
 package auction.bidders;
 
-import auction.Bidder;
+import auction.bidders.util.WinConditionStrategy;
+
+import java.util.Random;
+
+import static auction.bidders.util.Utils.isLastRound;
 
 /**
  * Works like {@link RandomBidder}, but re-calculates the average bid and standard deviation every time it places a bid.
  * Plus, it switches strategy if a win condition is met.
  */
-public class RandomBidder2 implements Bidder {
-    @Override
-    public void init(int quantity, int cash) {
-
-    }
-
+public class RandomBidder2 extends AbstractBidder {
     @Override
     public int placeBid() {
-        return 0;
-    }
+        // If a win condition is met, switch to the win condition strategy
+        if (WinConditionStrategy.isWinConditionMet(startQuantity, ownQuantity, ownCash, otherCash)) {
+            return WinConditionStrategy.getNextBid(startQuantity, ownQuantity, ownCash, otherCash);
+        }
+        // In the last round, bid all remaining cash
+        if (isLastRound(startQuantity, ownQuantity, otherQuantity)) {
+            return ownCash; // Bid all remaining cash in the last round
+        }
 
-    @Override
-    public void bids(int own, int other) {
-
+        int averageBid = (ownCash / (startQuantity - ownQuantity - otherQuantity)) * 2;
+        int standardDeviation = averageBid / 2; // 50% of the average bid
+        // Generate a random bid around the average bid with some variation
+        int randomBid = (int) (new Random().nextGaussian(averageBid, standardDeviation));
+        if (randomBid < 0) {
+            randomBid = 0; // Ensure we always bid at least 0
+        } else if (randomBid > ownCash) {
+            randomBid = ownCash; // Ensure we do not bid more than we have
+        }
+        return randomBid;
     }
 }
